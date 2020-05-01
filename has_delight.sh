7abc751a76ef15335a2307bad0ee893fbfc11935
pppest <ppest@protonmail.com>
2020-05-01 16:22:18 -0500

    has_delight.sh now gets latest release

--- has_delight.sh
+++ has_delight.sh
@@ -5,9 +5,10 @@ then
     echo "Delight exists on your filesystem."
 
 else
- wget https://github.com/devaultcrypto/DeLight/releases/download/v4.0.12/DeLight-4.0.12.tar.gz
- echo 'sdsd'
- tar -xvf DeLight-4.0.12.tar.gz
- mv DeLight-4.0.12 DeLight
+ VERSION=$(curl -s "https://github.com/devaultcrypto/DeLight/releases/latest" | grep -o 'tag/[v.0-9]*' | awk -F/ '{print $2}')
+ VERSION="${VERSION:1}"
+ wget https://github.com/devaultcrypto/DeLight/releases/download/v$VERSION/DeLight-$VERSION.tar.gz
+ tar -xvf ./DeLight-$VERSION.tar.gz
+ mv ./DeLight-$VERSION DeLight
  rm DeLight-4.0.12.tar.gz
 fi
