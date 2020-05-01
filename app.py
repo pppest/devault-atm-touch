e310e8975aa6d260bb6da0095aa1f3d34aaaeeba
pppest <ppest@protonmail.com>
2020-04-30 19:30:34 -0500

    .

--- app.py
+++ app.py
@@ -89,11 +89,10 @@ class AtmApp(App):
         self.dvt_bought = c.COINS/c.PRICE_WITH_FEE
         self.dvt_bought_str = str(round(c.COINS/c.PRICE_WITH_FEE, 3))
 
-        current_screen_id = self.root.current
-        self.root.ids.current_screen_id.export_to_png(time.time() + '.png')
 
         # deposit if timeout and dvt bought and wallet scanned
         if self.root.current == 'buy':
+            self.root.ids.buy.export_to_png(time.time() + '.png')
             #print(self.root.current)
             if (self.rl_time - self.start_time >= c.TIMEOUT):
                 print('TIMEOUT')
