25944bbb5860f38471dbb49a9bb6f7b96cb3b370
ok <ppest@protonmail.com>
2020-05-14 18:17:29 -0500

    .

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from kivy.uix.label import Label
 from kivy.uix.progressbar import ProgressBar
 from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.factory.Factory import Factory
 
 import time
 import os
